80b380ad76df71573d4fc593784a01f81fa800eb
sjx <1019876912@qq.com>
2020-04-28 10:25:40 +0800

    add loginsetting

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -14,7 +14,15 @@ function post() {
             if(response.code == 200) {
                 $("#comment_section").hide();
             } else {
-                alert(response.message);
+                if(response.code == 2003) {
+                    var isAccepted = confirm(response.message);
+                    if(isAccepted) {
+                        window.open("https://github.com/login/oauth/authorize?client_id=Iv1.f2ab5ec886f983ef&redirect_uri=http://localhost:8887/callback&scope=user&state=1");
+                        window.localStorage.setItem("closeable", true);
+                    }
+                } else {
+                    alert(response.message);
+                }
             }
             console.log(response);
         },

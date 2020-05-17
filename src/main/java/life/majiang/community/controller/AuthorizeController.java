78d95d3b0d94ee2250d53a86695576ba9fa06ad5
sjx <1019876912@qq.com>
2020-05-17 21:30:12 +0800

    add journal 2

--- src/main/java/life/majiang/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -5,6 +5,7 @@ import life.majiang.community.dto.GithubUser;
 import life.majiang.community.model.User;
 import life.majiang.community.provider.GithubProvider;
 import life.majiang.community.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -17,6 +18,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
+@Slf4j
 public class AuthorizeController {
     
     @Autowired
@@ -70,6 +72,7 @@ public class AuthorizeController {
             return "redirect:/";
             //登录成功，写cookie 和session
         } else {
+            log.error("callback get github error,{}", githubUser);
             //登录失败，重新登录
             return "redirect:/";
         }

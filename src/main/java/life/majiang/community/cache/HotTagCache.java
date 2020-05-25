b35f778eb1d5ab9ff48d2400b3133df4c473d8c8
sjx <1019876912@qq.com>
2020-05-25 09:46:54 +0800

    add hottagcache

---
+++ src/main/java/life/majiang/community/cache/HotTagCache.java
@@ -0,0 +1,13 @@
+package life.majiang.community.cache;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@Data
+public class HotTagCache {
+    private Map<String, Integer> tags = new HashMap<>();
+}
--- src/main/java/life/majiang/community/schedule/HotTagTasks.java
+++ src/main/java/life/majiang/community/schedule/HotTagTasks.java
@@ -1,17 +1,17 @@
 package life.majiang.community.schedule;
 
+import life.majiang.community.cache.HotTagCache;
 import life.majiang.community.mapper.QuestionMapper;
 import life.majiang.community.model.Question;
 import life.majiang.community.model.QuestionExample;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.ibatis.session.RowBounds;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 @Component
 @Slf4j
@@ -20,18 +20,40 @@ public class HotTagTasks {
     @Autowired
     private QuestionMapper questionMapper;
 
+    @Autowired
+    private HotTagCache hotTagCache;
+
     @Scheduled(fixedRate = 10000)
     public void hotTagSchedule() {
         int offset = 0;
         int limit = 20;
+        log.info("hotTagSchedule start {}", new Date());
         List<Question> list = new ArrayList<>();
+        Map<String, Integer> priorities = new HashMap<>();
         while(offset == 0 || list.size() == limit) {
             list = questionMapper.selectByExampleWithRowbounds(new QuestionExample(), new RowBounds(offset, limit));
             for(Question question : list) {
-                log.info("list question : {}", question.getId());
+                String[] tags = StringUtils.split(question.getTag(), ",");
+                for(String tag : tags) {
+                    Integer priority = priorities.get(tag);
+                    if(priority != null) {
+                        priorities.put(tag, priority + 5 + question.getCommentCount());
+                    } else {
+                        priorities.put(tag, 5 + question.getCommentCount());
+                    }
+                }
             }
             offset += limit;
         }
+        hotTagCache.setTags(priorities);
+        hotTagCache.getTags().forEach(
+                (k, v) -> {
+                    System.out.print(k);
+                    System.out.print(":");
+                    System.out.print(v);
+                    System.out.println();
+                }
+        );
         log.info("hotTagSchedule stop {}", new Date());
     }
 }

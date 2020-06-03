686bd14691adb6b3662738bc61e9d873e12e5f06
sjx <1019876912@qq.com>
2020-06-03 11:28:31 +0800

    add ad

--- src/main/java/life/majiang/community/interceptor/SessionInterceptor.java
+++ src/main/java/life/majiang/community/interceptor/SessionInterceptor.java
@@ -3,8 +3,10 @@ package life.majiang.community.interceptor;
 import life.majiang.community.mapper.UserMapper;
 import life.majiang.community.model.User;
 import life.majiang.community.model.UserExample;
+import life.majiang.community.service.AdService;
 import life.majiang.community.service.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,6 +14,7 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Service
@@ -23,8 +26,18 @@ public class SessionInterceptor implements HandlerInterceptor {
     @Autowired
     private NotificationService notificationService;
 
+    @Autowired
+    private AdService adService;
+
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //设置context级别属性
+        request.getServletContext().setAttribute("redirectUri", redirectUri);
+        //没有登录也可以查看导航
+        request.getServletContext().setAttribute("ads", adService.list());;
         Cookie[] cookies = request.getCookies();
         if(cookies != null && cookies.length != 0) {
             for (Cookie cookie : cookies) {
---
+++ src/main/java/life/majiang/community/service/AdService.java
@@ -0,0 +1,22 @@
+package life.majiang.community.service;
+
+import life.majiang.community.mapper.AdMapper;
+import life.majiang.community.model.Ad;
+import life.majiang.community.model.AdExample;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AdService {
+
+    @Autowired
+    private AdMapper adMapper;
+
+    public List<Ad> list() {
+        AdExample adExample = new AdExample();
+        adExample.createCriteria().andStatusEqualTo(1).andGmtStartLessThan(System.currentTimeMillis()).andGmtEndGreaterThan(System.currentTimeMillis());
+        return adMapper.selectByExample(adExample);
+    }
+}
